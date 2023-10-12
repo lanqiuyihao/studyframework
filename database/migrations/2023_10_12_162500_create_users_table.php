bb8a8e038ae79770cdc69bc44b79bc5d25aa607e
lanqiuyihao <lanqiuyihao@126.com>
2023-10-12 19:51:36 +0800

    删除框架不需要的文件和管理员表数据填充

--- database/migrations/2023_10_12_162500_create_users_table.php
+++ database/migrations/2023_10_12_162500_create_users_table.php
@@ -15,11 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('username',50)->comment('用户名');
+            $table->string('truename',20)->comment('真实姓名');
+            $table->string('password',255)->comment('密码');
+            $table->string('email',50)->comment('邮箱');
+            $table->enum('sex',['先生','女士'])->default('先生')->comment('先生');
+            $table->char('phone',15)->comment('手机');
+            $table->char('last_ip',15)->comment('最后登录ip');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
